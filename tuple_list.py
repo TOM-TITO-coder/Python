dc6c4ac91a4115f63489220f1a9c4b52c08bdec8
TOM-TITO <126035168+Tito-com@users.noreply.github.com>
2023-11-06 18:47:02 +0700

    added tuple_list.py

---
+++ tuple_list.py
@@ -0,0 +1,14 @@
+#Write a Python program that accepts a sequence of comma-separated numbers from the user and generates a list and a tuple of those numbers.
+#Sample data : 3, 5, 7, 23
+#Output :
+#List : ['3', ' 5', ' 7', ' 23']
+#Tuple : ('3', ' 5', ' 7', ' 23')
+
+value = input("Enter a sequence of comma-separated numbers: ")
+
+list = value.split(",")
+
+tuple = tuple(list)
+
+print("List: ", list)
+print("Tuple: ", tuple)
